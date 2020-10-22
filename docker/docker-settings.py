97482f5dd4fd3547adaf27c15a4f0a9d0c79d14e
Jaime <jailucloz@alum.us.es>
2020-10-22 17:06:04 +0200

    cambios docker-settings

--- docker/docker-settings.py
+++ docker/docker-settings.py
@@ -1,42 +1,45 @@
-DEBUG = True
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
-}
-
-STATIC_ROOT = '/app/static/'
-MEDIA_ROOT = '/app/static/media/'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 # Modules in use, commented modules that you won't use
 MODULES = [
-    'authentication',
-    'base',
-    'booth',
-    'census',
-    'mixnet',
-    'postproc',
-    'store',
-    'visualizer',
-    'voting',
+	'authentication',
+	'base',
+	'booth',
+	'census',
+	'mixnet',
+	'postproc',
+	'store',
+	'visualizer',
+	'voting',
 ]
 
-BASEURL = 'http://10.5.0.1:8000'
-
 APIS = {
-    'authentication': 'http://10.5.0.1:8000',
-    'base': 'http://10.5.0.1:8000',
-    'booth': 'http://10.5.0.1:8000',
-    'census': 'http://10.5.0.1:8000',
-    'mixnet': 'http://10.5.0.1:8000',
-    'postproc': 'http://10.5.0.1:8000',
-    'store': 'http://10.5.0.1:8000',
-    'visualizer': 'http://10.5.0.1:8000',
-    'voting': 'http://10.5.0.1:8000',
+	'authentication': 'http://localhost:8000',
+	'base': 'http://localhost:8000',
+	'booth': 'http://localhost:8000',
+	'census': 'http://localhost:8000',
+	'mixnet': 'http://localhost:8000',
+	'postproc': 'http://localhost:8000',
+	'store': 'http://localhost:8000',
+	'visualizer': 'http://localhost:8000',
+	'voting': 'http://localhost:8000',
 }
+
+BASEURL = 'http://localhost:8000'
+
+DATABASES = {
+	'default': {
+    	'ENGINE': 'django.db.backends.postgresql',
+    	'NAME': 'decide',
+    	'USER': 'decide',
+    'PASSWORD': 'decide',
+    	'HOST': 'localhost',
+    	'PORT': '5432',
+	}
+}
+
+# number of bits for the key, all auths should use the same number of bits
+KEYBITS = 256
+
+STATIC_URL = '/static/'
+
